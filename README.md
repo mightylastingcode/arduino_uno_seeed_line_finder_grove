42ff96b253ca21ab8ebb224aa4b80c9ac88b1b12
Michael Li <mikelisfbay@yahoo.com>
2019-10-27 14:50:54 -0700

    add code and doc

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # arduino_uno_seeed_line_finder_grove
 Seeed Line Finder Grove.  One logic output 1-black surface, 0-white line
+
+<img src="linefinder.png" style="width:80%"> 
---
+++ Seeed_line_finder_grove_interrupt/Seeed_line_finder_grove_interrupt.ino
@@ -0,0 +1,43 @@
+//
+// Seeed line finder (grove interface)
+
+// red   5V
+// black gnd
+// white NC
+// yellow D3 (Output) (Hint: UNO input)
+
+// D3 1 means black surface
+//    0 means white line
+
+int senseLine = 3; // (D3)
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  
+  // Setup digital input to get the digital output
+  // of the line finder sensor.
+  Serial.print("Setup D");
+  Serial.print(senseLine);
+  Serial.println(" for the line finder sensor."); 
+  
+  pinMode(senseLine, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(senseLine), ColorChange, CHANGE);
+}
+
+void loop() {
+  // Do Nothing
+  delay(100);
+
+}
+
+void ColorChange() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(senseLine))
+    Serial.println("Black Surface!");
+  else 
+    Serial.println("White Line!");
+}
